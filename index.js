9d02efe13db31a0382fd750c7ef05bcfa8c2c3f5
Rudy Palfray <rpalfray87@gmail.com>
2023-05-30 15:39:19 +0200

    Refactoring Bot and create start method

--- index.js
+++ index.js
@@ -1,113 +1,18 @@
-import fs from 'fs';
 import { Client, Events, GatewayIntentBits } from 'discord.js';
 import * as dotenv from 'dotenv'
 import Bot from './src/Bot.js';
 
 dotenv.config()
 
-const fileDirectory = "./Ressources/";
 const token = process.env.BOT_TOKEN;
-const client = new Client({ intents: [GatewayIntentBits.Guilds] }); // Create a new client instance
+const client = new Client({ intents: [GatewayIntentBits.Guilds] }); // 
 
-/*
-const fileName = "message.txt";
-
-const content = fs.readFileSync(fileName, 'utf-8');
-
-//onUpdate file function    
-fs.watchFile(fileName, (curr, prev) => {
-    console.log("content updated");
-});
-
-console.log(content);
-*/
-
-
-// When the client is ready, run this code (only once)
-// We use 'c' for the event parameter to keep it separate from the already defined 'client'
 client.once(Events.ClientReady, c => {
 	console.log(`Ready! Logged in as ${c.user.id}`);
-    const bot = new Bot(c.user.id);
-    const ressourcesId = getRessourcesId();
-    listChannelsFromParentId(ressourcesId);
-
-    /*(async () => {
-        let msg = await recoverMessageFromChannelId("1112046183750307881");
-        console.log(msg)
-        editMessageInChannelId("1112046183750307881", msg.id, "Hello je suis le bot et je peux me modifier <3");
-    })()*/
-    
-    //writeMessageInChannelId("1112046183750307881", "Hello je suis le bot et je parle <3");
+    const channels = client.channels.cache;
+    const bot = new Bot(c.user.id, channels);
+    bot.start();
 });
-
-
-
-
-// recover ressources channel id
-function getRessourcesId() {
-    const channels = client.channels.cache;
-    let rid = "";
-    channels.forEach(channel => {
-        if(channel.name === "Ressources") {
-            rid = channel.id;
-            return
-        }
-    });
-    return rid;
-}
-
-// list channels from parent id
-function listChannelsFromParentId(id){
-    const channels = client.channels.cache;
-    channels.forEach(channel => {
-        if(channel.parentId == id) {
-            console.log("Name : " + channel.name + " id : " + channel.id);
-            recoverTxtFileFromChannelName(channel.name)
-        }
-    });
-}
-
-function recoverTxtFileFromChannelName(channelName) {
-    if(fs.existsSync(fileDirectory + channelName+".txt")) {
-        const content = fs.readFileSync(fileDirectory + channelName+".txt", 'utf-8');
-        console.log(content)
-    } else {
-        createFile(channelName+".txt")
-    }
-}
-
-function createFile(fileName) {
-    fs.writeFile("./Ressources/" + fileName, 'utf-8', (err) => {
-        if (err) throw err;
-        console.log('The file has been saved!');
-    });
-}
-
-async function recoverMessageFromChannelId(id) {
-    const channel = client.channels.cache.get(id);
-    let message = null;
-    await channel.messages.fetch().then(messages => {
-        messages.forEach(msg => {
-            if(msg.author.id === client.user.id) {
-                console.log("Content : " + msg.content)
-                message = msg
-            }
-        });
-    });
-    return message
-}
-
-function writeMessageInChannelId(id, message) {
-    const channel = client.channels.cache.get(id);
-    channel.send(message);
-}
-
-function editMessageInChannelId(id, messageId, messageContent) {
-    const channel = client.channels.cache.get(id);
-    channel.messages.fetch(messageId).then(message => {
-        message.edit(messageContent);
-    });
-}
 
 // Log in to Discord with your client's token
 client.login(token);
--- src/Bot.js
+++ src/Bot.js
@@ -1,8 +1,115 @@
+import { channel } from 'diagnostics_channel';
+import fs from 'fs';
+
 export default class Bot {
-    constructor(id) {
+    constructor(id, channels) {
         this.id = id;
+        this.channels = channels;
+        this.fileDirectory = "./Ressources/";
+        this.extensionFile = ".txt";
+        this.ressourcesId = this.getRessourcesId();
+        this.channelsList = this.listChannelsFromParentId(this.ressourcesId);
     }
     getId() {
         return this.id;
+    }
+
+    // recover ressources channel id
+    getRessourcesId() {
+        let rid = "";
+        this.channels.forEach(channel => {
+            if(channel.name === "Ressources") {
+                rid = channel.id;
+                return
+            }
+        });
+        return rid;
+    }
+
+    listChannelsFromParentId(id){
+        const channelsList = [];
+        this.channels.forEach(channel => {
+            if(channel.parentId == id) {
+                //console.log("Name : " + channel.name + " id : " + channel.id);
+                channelsList.push({name : channel.name, id : channel.id});
+                //recoverTxtFileFromChannelName(channel.name)
+            }
+        });
+        return channelsList;
+    }
+
+    // check if file already exist, if not create it
+    checkFiles() {
+        this.channelsList.forEach(channel => {
+            if(!fs.existsSync(this.fileDirectory + channel.name + this.extensionFile)) {
+                console.log("Create file : " + channel.name + this.extensionFile)
+                this.createFile(channel.name + this.extensionFile)
+            }
+        });
+    }
+
+    // recover discord message from channel id
+    async recoverMessageFromChannelId(channelId) {
+        const channel = this.channels.get(channelId);
+        let message = null;
+        await channel.messages.fetch().then(messages => {
+            messages.forEach(msg => {
+                if(msg.author.id === this.id) {
+                    message = msg
+                }
+            });
+        });
+        return message
+    }
+
+    // start the bot
+    async start() {
+        console.log("Bot started");
+        //Check if file already exist for all channels, if not create it
+        await this.checkFiles()
+        this.channelsList.forEach(channel => {
+            fs.watchFile(this.fileDirectory + channel.name + this.extensionFile, (curr, prev) => {
+                console.log("Content updated from : " + channel.name + this.extensionFile);
+                this.recoverMessageFromChannelId(channel.id).then(msg => {
+                    const contentTxtFile = this.recoverContentFileFromChannelName(channel.name)
+                    if (msg != null) {
+                        this.editMessageInChannelId(channel.id, msg.id, contentTxtFile);
+                    } else {
+                        this.writeMessageInChannelId(channel.id, contentTxtFile)
+                    }
+                });
+            });
+        })
+    }
+
+    // recover content from file for a specific channel
+    recoverContentFileFromChannelName(channelName) {
+        const file = this.fileDirectory + channelName + this.extensionFile
+        if(fs.existsSync(file)) {
+            const content = fs.readFileSync(file, 'utf-8');
+            return content;
+        }
+    }
+
+    // create a new file
+    createFile(fileName) {
+        fs.writeFile(this.fileDirectory + fileName, 'utf-8', (err) => {
+            if (err) throw err;
+            console.log('The file has been saved!');
+        });
+    }
+
+    // write a new message in channel
+    writeMessageInChannelId(id, message) {
+        const channel = this.channels.get(id);
+        channel.send(message);
+    }
+
+    // edit a message in channel if message already exist
+    editMessageInChannelId(id, messageId, messageContent) {
+        const channel = this.channels.get(id);
+        channel.messages.fetch(messageId).then(message => {
+            message.edit(messageContent);
+        });
     }
 }
